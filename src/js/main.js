e396f2d360eec898e2ca4ae571ad52f18ef2718e
teeppope <terraepop@gmail.com>
2016-09-28 11:47:02 -0700

    adjust typewriter timing

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,21 @@ import "../styles/style.scss";
 
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [
+  const dataText = [
 	  				"mobile first development\"", 
-	  				"design thinking\"", 
 	  				"pixel perfection\"", 
-	  				"DRY code\"",
-	  				"Github\""
+	  				"design thinking\"", 
+	  				"DRY coding\"",
+	  				"Github\"",
+	  				"using build tools\""
   				];
-  
-  // console.log(dataText);
   // type one text in the typwriter
   // keeps calling itself until the text is finished
   function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
+    // check if text isn't finished yet
     if (i < (text.length)) {
-      // add next character to h1
-     document.querySelector(".teep-writer").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+      // add next character to teep-writer
+     document.querySelector(".teep-writer").innerHTML = text.substring(0, i+1);
 
       // wait for a while and call this function again for next character
       setTimeout(function() {
@@ -29,7 +28,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     // text finished, call callback if there is a callback function
     else if (typeof fnCallback == 'function') {
       // call callback after timeout
-      setTimeout(fnCallback, 700);
+      setTimeout(fnCallback, 3000);
     }
   }
   // // start a typewriter animation for a text in the dataText array
@@ -37,7 +36,7 @@ document.addEventListener('DOMContentLoaded',function(event){
      if (typeof dataText[i] == 'undefined'){
         setTimeout(function() {
           StartTextAnimation(0);
-        }, 1000);
+        }, 500);
      }
      // check if dataText[i] exists
     if (i < dataText[i].length) {
@@ -45,6 +44,7 @@ document.addEventListener('DOMContentLoaded',function(event){
      typeWriter(dataText[i], 0, function(){
        // after callback (and whole text has been animated), start next text
        StartTextAnimation(i + 1);
+
      });
     }
   }
