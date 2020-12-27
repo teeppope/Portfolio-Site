a8408c171a2697e48a366b0407f391c4a615abb2
Terra Pope <terraepop@gmail.com>
2020-12-27 13:47:33 -0800

    upgrading webpack config/versions, fixing build errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const PATHS = {
 	src: path.resolve(__dirname, 'src'),
@@ -9,31 +9,60 @@ const PATHS = {
 };
 
 module.exports = {
+	mode: 'development',
 	entry: path.resolve(PATHS.src, 'js/main.js'),
-	output: {
-		path: PATHS.dist,
-		filename: 'bundle.[hash].js'
+	devServer: {
+		contentBase: './dist',
 	},
 	module: {
-		loaders: [
+		rules: [
 			{
-				test: /\.(css|scss)$/,
-				loader: ExtractTextPlugin.extract(['css', 'sass'])
+				test: /\.(css|s[ac]ss)$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				  ]
 			},
 			{
-				test: /\.js$/,
-				loader: 'babel'
-			}
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+				loader: "babel-loader",
+				options: {
+					presets: ['@babel/preset-env']
+				}
+				}
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				use: [
+				  {
+					loader: 'file-loader',
+				  },
+				],
+			},
 		]
 	},
 	plugins: [
+		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: path.resolve(PATHS.src, 'index.html')
 		}),
-		new ExtractTextPlugin('style.[hash].css'),
-		new CopyWebpackPlugin([{
-			from: path.resolve(PATHS.src, 'assets'), 
-			to: path.resolve(PATHS.dist, 'assets')
-		}])
-	]
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(PATHS.src, 'assets'), 
+					to: path.resolve(PATHS.dist, 'assets')
+				}
+			],
+		}),
+	],
+	output: {
+		path: PATHS.dist,
+		filename: 'bundle.[fullhash].js'
+	},
 }
