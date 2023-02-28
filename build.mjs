23c930553ed458d6460239f2c969e0e4c1af974a
Terra Pope <terraepop@gmail.com>
2023-02-27 21:32:15 -0800

    switch to esbuild for build tool, and work on getting a new styleguide setup and styled

---
+++ build.mjs
@@ -0,0 +1,15 @@
+import * as esbuild from 'esbuild'
+
+let ctx = await esbuild.context({
+  entryPoints: [
+    {out: 'main', in: './src/js/main.js'},
+    {out: 'styles', in: './src/styles/app.css'},
+],
+  bundle: true,
+  target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+  loader: { '.woff': 'copy', '.woff2': 'copy', '.svg': 'dataurl' },
+  outdir: 'dist',
+});
+
+await ctx.watch();
+console.log('watching...');
