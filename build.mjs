528b000b88526ac0f02d321cef14b22c6da9cf39
Terra Pope <terraepop@gmail.com>
2023-05-02 13:47:38 -0700

    stylesheet updates and start on homepage

--- build.mjs
+++ build.mjs
@@ -1,14 +1,14 @@
 import * as esbuild from 'esbuild'
 
 let ctx = await esbuild.context({
-  entryPoints: [
-    {out: 'main', in: './src/js/main.js'},
-    {out: 'styles', in: './src/styles/app.css'},
-],
-  bundle: true,
-  target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-  loader: { '.woff': 'copy', '.woff2': 'copy', '.svg': 'dataurl' },
-  outdir: 'dist',
+    entryPoints: [
+        {out: 'main', in: './src/js/main.js'},
+        {out: 'styles', in: './src/styles/app.css'},
+    ],
+    bundle: true,
+    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    loader: { '.woff': 'copy', '.woff2': 'copy', '.svg': 'dataurl' },
+    outdir: 'dist',
 });
 
 await ctx.watch();
